9d1d28049ebaee65f8cd9fdd2d903af6a559e3e5
sk081-sk <60762218+sk081-sk@users.noreply.github.com>
2025-04-10 10:51:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,12 @@ session=cnx.session()
 # my_dataframe = session.table("smoothies.public.fruit_options")
 # my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function 
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect (
